2566f31460073496f214fa71ef97f08e43c8de9b
Denilson Semedo Tavares <dstavares00@gmail.com>
2022-03-27 17:33:53 +0100

    all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,29 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'calculadora_final';
+  input1 = "";
+  input2 = "";
+  result = 0;
+  num1=0;
+  num2=0;
+
+  sum():void{
+    this.result = parseInt(this.input1) + parseInt(this.input2)
+  }
+
+  sub():void{
+    this.result = parseInt(this.input1) - parseInt(this.input2)
+  }
+
+  mult():void{
+    this.result = parseInt(this.input1) * parseInt(this.input2)
+  }
+
+  div():void{
+    this.result = parseInt(this.input1) / parseInt(this.input2)
+  }
 }
